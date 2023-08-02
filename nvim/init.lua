588acd8438813bd9ab4af321f63d20848482a11b
kashishg <kashg@seas.upenn.edu>
2023-08-02 11:16:11 -0400

    Added auto launch xml detection

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,4 +10,13 @@ vim.cmd("set expandtab")
 -- when indenting with '>', use 2 spaces width
 vim.cmd("set shiftwidth=2")
 
+-- Function to set the filetype to "xml" for .launch files
+local function set_launch_filetype()
+    if vim.fn.expand('%:e') == 'launch' then
+        vim.cmd('setfiletype xml')
+    end
+end
+
+set_launch_filetype()
+
 
